0e6dc72cf04ebd660d3d1f0d45a40e8f1bb25fd9
stellaexp <kimshillam@icloud.com>
2020-02-29 18:27:51 +0000

    add first page routing and set up connection to mongodb

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'recipe_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tdr956P@myfirstcluster-v3wwf.mongodb.net/recipe_manager?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+
 @app.route('/')
-def hello():
-    return "Hello, world"
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
