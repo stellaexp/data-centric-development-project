9eca7daa0dfe434d924793c5a304a064b48e29e4
stellaexp <kimshillam@icloud.com>
2020-04-20 17:06:56 +0000

    create index and welcome page with username session object

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -8,10 +8,33 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'recipes'
 app.config["MONGO_URI"] = 'mongodb+srv://root:r00tdr956P@myfirstcluster-v3wwf.mongodb.net/recipes?retryWrites=true&w=majority'
 
+app.secret_key = "randomstringneedstobechanged"
+
 mongo = PyMongo(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    """Username entry page"""
+    if request.method == "GET":
+        session.clear()
+
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        username = session['username']
+        return render_template("welcome.html", username=username)
+
+    return render_template("index.html")
+
+
+@app.route('/welcome')
+def welcome():
+    username = session['username']
+    return render_template("welcome.html", username=username)
+
+
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("recipes.html", recipe=mongo.db.recipe.find())
